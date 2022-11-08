fca3d2961b843ac1ec4ec642e27602f3fc32da87
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-11-08 17:09:23 +0900

    05

--- main.c
+++ main.c
@@ -5,8 +5,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char str[30] = "happy C programming";
+	char str[30];
+	FILE* fp;
+	fp = fopen("sample.txt","w");
 	
-	printf("length of %s: %i\n",str,strlen(str)); 
+	printf("input a word:");
+	scanf("%s",str);
+	fprintf(fp,"%s\n",str);
+	
+	printf("input a word:");
+	scanf("%s",str);
+	fprintf(fp,"%s\n",str);
+	
+	printf("input a word:");
+	scanf("%s",str);
+	fprintf(fp,"%s\n",str);
+	
+	fclose(fp);
+	
 	return 0;
 }
